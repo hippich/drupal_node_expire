2defd7303b61d5dafa82a42fba71a0590a69bf31
Bruno Massa <brmassa@67164.no-reply.drupal.org>
2009-01-29 21:01:33 +0000

    New features: * Max expiration date.

--- node_expire.js
+++ node_expire.js
@@ -1,5 +1,16 @@
 // $Id$
 
 Drupal.behaviors.nodeExpire = function(context) {
-  $("#edit-expire").datepicker({dateFormat: Drupal.settings.dateFormat});
+//   var min = new Date(Drupal.settings.minDate[0], Drupal.settings.minDate[1], Drupal.settings.minDate[3]);
+  var max = Drupal.settings.maxDate;
+  if (max) {
+    max = new Date(max[0], max[1] -1, max[2]);
+  }
+  var min = Drupal.settings.minDate;
+  min = new Date(min[0], min[1] -1, min[2]);
+  $("#edit-expire").datepicker({
+    dateFormat: Drupal.settings.dateFormat,
+    minDate:    min,
+    maxDate:    max
+  });
 }
